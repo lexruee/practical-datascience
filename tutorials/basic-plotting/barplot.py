69eb18892ef48aa289417f0ac6385f4a71183a2b
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-03-08 23:43:23 +0100

    add bar plot
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

---
+++ tutorials/basic-plotting/barplot.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from matplotlib import pyplot as plt
+
+movies = ['Annie Hall', 'Ben-Hur', 'Casablanca', 'Ghandi', 'West Side Story']
+num_oscars = [5, 11, 3, 8, 10]
+xs = [i + 0.1 for i, _ in enumerate(movies)]
+
+
+plt.bar(xs, num_oscars)
+plt.title('Movies')
+plt.ylabel('# of Academy Awards')
+plt.xlabel('Movies')
+plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+plt.show()
