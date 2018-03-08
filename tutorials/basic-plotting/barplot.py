e3deeb89376a3fb747228cd0b66015d8073a3920
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-03-08 23:46:09 +0100

    update bar plot
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- tutorials/basic-plotting/barplot.py
+++ tutorials/basic-plotting/barplot.py
@@ -4,12 +4,12 @@ from matplotlib import pyplot as plt
 
 movies = ['Annie Hall', 'Ben-Hur', 'Casablanca', 'Ghandi', 'West Side Story']
 num_oscars = [5, 11, 3, 8, 10]
-xs = [i + 0.1 for i, _ in enumerate(movies)]
+xs = [i for i, _ in enumerate(movies)]
 
 
 plt.bar(xs, num_oscars)
 plt.title('Movies')
 plt.ylabel('# of Academy Awards')
 plt.xlabel('Movies')
-plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+plt.xticks([i for i, _ in enumerate(movies)], movies)
 plt.show()
