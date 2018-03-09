e256eeac39c20a3cea9f7c178af67024efa33221
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-03-09 02:27:42 +0100

    rename helper module
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- tutorials/basic-statistics/statistics.py
+++ tutorials/basic-statistics/statistics.py
@@ -3,7 +3,7 @@
 from matplotlib import pyplot as plt
 from collections import Counter
 import random
-import stats
+import helper
 
 xs = [random.uniform(0, 100) for _ in range(0, 100)]
 ys = [random.uniform(0, 100) for _ in range(0, 100)]
@@ -11,11 +11,11 @@ ys = [random.uniform(0, 100) for _ in range(0, 100)]
 min_xs = min(xs)
 max_xs = max(xs)
 length = len(xs)
-mean_xs = stats.mean(xs)
-variance_xs = stats.variance(xs)
-std_xs = stats.std(xs)
-median_xs = stats.median(xs)
-data_range_xs = stats.data_range(xs)
+mean_xs = helper.mean(xs)
+variance_xs = helper.variance(xs)
+std_xs = helper.std(xs)
+median_xs = helper.median(xs)
+data_range_xs = helper.data_range(xs)
 
 print("min: %s" % min_xs)
 print("max: %s" % max_xs)
@@ -27,5 +27,5 @@ print("median: %s" % median_xs)
 print("range: %s" % data_range_xs)
 
 
-covariance_xsys = stats.covariance(xs, ys)
+covariance_xsys = helper.covariance(xs, ys)
 print("covariance: %s" % covariance_xsys)
