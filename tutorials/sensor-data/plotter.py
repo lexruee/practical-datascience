b48c8bd81c0f80f422af94727a0d56f8343520e6
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2018-03-18 09:43:16 +0100

    implement sma feature
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- tutorials/sensor-data/plotter.py
+++ tutorials/sensor-data/plotter.py
@@ -9,11 +9,14 @@ from datetime import datetime
 import argparse
 
 
-def create_plot(data):
+def create_plot(data, ma):
     temperature = []
     humidity= []
     pressure = []
     timestamp = []
+    sma = {'temperature': [], 'humidity': [], 'pressure': [], 'time':[] }
+
+
     for row in data:
         temperature.append(row['temperature'])
         humidity.append(row['humidity'])
@@ -22,6 +25,17 @@ def create_plot(data):
 
     time = [datetime.fromtimestamp(t) for t in timestamp]
     min_time, max_time = min(time), max(time)
+
+    if ma > 0:
+        for i in range(0, len(time)-ma, ma):
+            p_ma = sum(pressure[i:i+ma])/ma
+            t_ma = sum(temperature[i:i+ma])/ma
+            h_ma = sum(humidity[i:i+ma])/ma
+            sma['temperature'].append(t_ma)    
+            sma['pressure'].append(p_ma)    
+            sma['humidity'].append(h_ma)    
+            t = datetime.fromtimestamp((timestamp[i] + timestamp[i+ma])/2)    
+            sma['time'].append(t)
 
     hfmt = dates.DateFormatter('%m/%d %H:%M')
     fig, (ax1, ax2, ax3) = plt.subplots(3, sharex=True, sharey=False)
@@ -33,17 +47,17 @@ def create_plot(data):
     ax1.xaxis.set_minor_locator(dates.MinuteLocator(interval=15))
     ax1.xaxis.set_major_formatter(hfmt)
     ax1.set_xticks(np.arange(0,24,1))
-    ax1.plot(time, temperature)
+    ax1.plot(time, temperature, '--b', sma['time'], sma['temperature'], '--r')
     ax1.set_xlabel('Time')
     ax1.set_ylabel('Temperature in C')
     #ax1.set_xlim(min(time),max(time))
 
-
-    ax2.plot(time, humidity)
+    ax2.plot(time, humidity, '--b', sma['time'], sma['humidity'], '--r')
     ax2.set_xlabel('Time')
     ax2.set_ylabel('Humidity in percentage')
 
-    ax3.plot(time, pressure)
+    ax3.plot(time, pressure, sma['time'], sma['pressure'], '--r')
+
     ax3.set_xlabel('Time')
     ax3.set_ylabel('Pressure in Pa')
 
@@ -51,6 +65,8 @@ def create_plot(data):
     for ax in fig.axes:
         plt.sca(ax)
         ax.grid()
+        if ma > 0:
+            ax.legend(['Actual', 'SMA(%s)' % ma])
 
     plt.xticks(rotation=45)
     
@@ -71,13 +87,15 @@ if __name__ == '__main__':
             help='JSON input file.')
     parser.add_argument('-S', '--show', dest='show', 
                 help='Show plot.', action='store_true')
-  
+    parser.add_argument('-m', '--ma', dest='ma', type=int, default=10,
+            help='Compute and plot MA(p) where p is the order of the moving average.')
+   
     args = parser.parse_args()
  
-    with open('sensor-data-17-3-2018.json', 'r') as f:
+    with open(args.input, 'r') as f:
         data = json.loads(f.read())
 
-    plt = create_plot(data)
+    plt = create_plot(data, args.ma)
      
     if args.output:
         plt.savefig(args.output)
